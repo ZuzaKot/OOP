32606c31c268b38e354690a82f714ea07ee02dc4
ZuzaKot <75215467+ZuzaKot@users.noreply.github.com>
2022-08-29 22:28:14 +0200

    vyhledavacTextu
    cpp + h

---
+++ text2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string>
+#include "text2.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void Text::text(string CisloCviceni) {
+	string hledane = CisloCviceni;
+	string line;
+	fstream f("C:\\Users\\artcr\\Documents\\B_OOP\\cviceni\\texmod_vysledek.txt", ios::in);
+	if (!f) cout << "Chyba! Neslo otevrit dokument: texmod_vysledek.txt\n" << endl;
+	else {
+		while (getline(f, line)) {
+			if ((line[0] == hledane[0]) && (line[1] == hledane[1]) && (line[2] == hledane[2])) { 
+				cout << line << "\n" << endl; 
+				break; 
+			}
+		}
+	}
+	f.close();
+};
---
+++ vyhledavacTextu.cpp
@@ -0,0 +1,24 @@
+﻿// Tento zdrojový kód jsem vypracovala zcela samostatně bez cizí pomoci.
+// Neokopírovala jsem, ani jsem neopsala cizí zdrojové kódy.
+// Nikdo mi při vypracování neradil.
+// Pokud některý řádek porušuje toto pravidlo, je označen komentářem NENÍ MOJE TVORBA.
+// Porušení těchto pravidel se považuje za podvod, který lze potrestat VYLOUČENÍM ZE STUDIA.
+// Zuzana Kotásková, 36777
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include "text2.h"
+using namespace std;
+
+int main()
+{
+	Text Cviceni;
+	Cviceni.text("1.4");
+	Cviceni.text("2.2");
+	Cviceni.text("3.1");
+	Cviceni.text("4.1");
+	Cviceni.text("8.4");
+	Cviceni.text("9.1");
+}
