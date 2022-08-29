7d9421900809cda2eeb34df5238fc1f8245adbe3
ZuzaKot <75215467+ZuzaKot@users.noreply.github.com>
2022-08-29 22:23:55 +0200

    Cviceni_8_3
    cpp + h

---
+++ Cviceni_8_3.cpp
@@ -0,0 +1,24 @@
+﻿// Tento zdrojový kód jsem vypracovala zcela samostatně bez cizí pomoci.
+// Neokopírovala jsem, ani jsem neopsala cizí zdrojové kódy.
+// Nikdo mi při vypracování neradil.
+// Pokud některý řádek porušuje toto pravidlo, je označen komentářem NENÍ MOJE TVORBA.
+// Porušení těchto pravidel se považuje za podvod, který lze potrestat VYLOUČENÍM ZE STUDIA.
+// Zuzana Kotásková, 36777
+
+#include <iostream>
+#include "Header.h"
+
+int main()
+{
+    Integer a;
+    a.val(257);
+    a.show();
+
+    Double b;
+    b.val(36 * 36);
+    b.show();
+
+    Char c;
+    c.val('F');
+    c.show();
+}
--- Header.h
+++ Header.h
@@ -1,23 +1,32 @@
 #pragma once
+#include <iostream>
 using namespace std;
 
-namespace Pokus {
-	int a = 1;
-	int Pokus_a() { cout << a << endl; return a; };
-	int f() { cout << "namespace Pokus f(): " << a << endl; return a; };
-	int g() { cout << "namespace Pokus g(): " << (a * a) << endl; return a * a; };
+class Konzole {
+public:
+	Konzole() { };
 };
 
-namespace Dalsi_pokus {
-	int a = 2;
-	int Dalsi_pokus_a() { cout << a <<endl; return a; };
-	int f() { cout << "namespace Dalsi_pokus f(): " << a << endl; return a; };
-	int g() { cout << "namespace Dalsi_pokus g(): " << (a * a) << endl; return a * a; };
+class Integer :public Konzole {
+private:
+	int i; 
+public:
+	virtual void show() { cout << "Integer: " << i << endl; };
+	virtual void val(int a) { i = a; };
 };
 
-namespace Jeste_dalsi_pokus {
-	int a = 3;
-	int Jeste_dalsi_pokus_a() { cout << a << endl; return a; };
-	int f() { cout << "namespace Jeste_dalsi_pokus f(): "<< a << endl; return a; };
-	int g() { cout << "namespace Jeste_dalsi_pokus g(): " << (a * a) << endl; return a * a; };
+class Double :public Konzole {
+private:
+	double i;
+public:
+	virtual void show() { cout << "Double: " << i << endl; };
+	virtual void val(double a) { i = a; };
+};
+
+class Char :public Konzole {
+private:
+	char i;
+public:
+	virtual void show() { cout << "Char: " << i << endl; };
+	virtual void val(char a) { i = a; };
 };
