79aa0b6f326ca866cbfc8d5422f52d45c59e3977
ZuzaKot <75215467+ZuzaKot@users.noreply.github.com>
2022-08-29 22:23:33 +0200

    Cviceni_8_2
    cpp + h

---
+++ Cviceni_8_2.cpp
@@ -0,0 +1,27 @@
+﻿// Tento zdrojový kód jsem vypracovala zcela samostatně bez cizí pomoci.
+// Neokopírovala jsem, ani jsem neopsala cizí zdrojové kódy.
+// Nikdo mi při vypracování neradil.
+// Pokud některý řádek porušuje toto pravidlo, je označen komentářem NENÍ MOJE TVORBA.
+// Porušení těchto pravidel se považuje za podvod, který lze potrestat VYLOUČENÍM ZE STUDIA.
+// Zuzana Kotásková, 36777
+
+#include <iostream>
+#include "Header.h"
+using namespace Pokus;
+using namespace Dalsi_pokus;
+using namespace Jeste_dalsi_pokus;
+
+int main()
+{
+    Pokus_a();
+    Pokus::a = 9; Dalsi_pokus::a = 15;
+    Pokus::f();
+    Pokus::g();
+    Dalsi_pokus_a();
+    Dalsi_pokus::f();
+    Dalsi_pokus::g();
+    Jeste_dalsi_pokus_a();
+    Jeste_dalsi_pokus::f();
+    Jeste_dalsi_pokus::g();
+
+}
--- Header.h
+++ Header.h
@@ -1,21 +1,23 @@
 #pragma once
+using namespace std;
+
 namespace Pokus {
 	int a = 1;
-	int Pokus_a() { printf("%d\n", a); return a; };
-	int f() { printf("namespace Pokus f(): %d\n", a); return a; };
-	int g() { printf("namespace Pokus g(): %d\n", a * a); return a * a; };
+	int Pokus_a() { cout << a << endl; return a; };
+	int f() { cout << "namespace Pokus f(): " << a << endl; return a; };
+	int g() { cout << "namespace Pokus g(): " << (a * a) << endl; return a * a; };
 };
 
 namespace Dalsi_pokus {
-	int b = 2;
-	int Dalsi_pokus_a() { printf("%d\n", b); return b; };
-	int f() { printf("namespace Dalsi_pokus f(): %d\n", b); return b; };
-	int g() { printf("namespace Dalsi_pokus g(): %d\n", b * b); return b * b; };
+	int a = 2;
+	int Dalsi_pokus_a() { cout << a <<endl; return a; };
+	int f() { cout << "namespace Dalsi_pokus f(): " << a << endl; return a; };
+	int g() { cout << "namespace Dalsi_pokus g(): " << (a * a) << endl; return a * a; };
 };
 
 namespace Jeste_dalsi_pokus {
-	int c = 3;
-	int Jeste_dalsi_pokus_a() { printf("%d\n", c); return c; };
-	int f() { printf("namespace Jeste_dalsi_pokus f(): %d\n", c); return c; };
-	int g() { printf("namespace Jeste_dalsi_pokus g(): %d\n", c * c); return c * c; };
+	int a = 3;
+	int Jeste_dalsi_pokus_a() { cout << a << endl; return a; };
+	int f() { cout << "namespace Jeste_dalsi_pokus f(): "<< a << endl; return a; };
+	int g() { cout << "namespace Jeste_dalsi_pokus g(): " << (a * a) << endl; return a * a; };
 };
