1ff56c15178ec140e6eda94957c0287636918d4d
ZuzaKot <75215467+ZuzaKot@users.noreply.github.com>
2022-08-29 22:26:02 +0200

    Cviceni_9_4
    cpp + h

---
+++ Cviceni_9_4.cpp
@@ -0,0 +1,48 @@
+﻿// Tento zdrojový kód jsem vypracovala zcela samostatně bez cizí pomoci.
+// Neokopírovala jsem, ani jsem neopsala cizí zdrojové kódy.
+// Nikdo mi při vypracování neradil.
+// Pokud některý řádek porušuje toto pravidlo, je označen komentářem NENÍ MOJE TVORBA.
+// Porušení těchto pravidel se považuje za podvod, který lze potrestat VYLOUČENÍM ZE STUDIA.
+// Zuzana Kotásková, 36777
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include "text.h"
+#include "template.h"
+using namespace std;
+// ROZDĚLANÉ....ZATÍM VYHAZUJE CHYBY (přetečení)....
+
+int main()
+{ 
+	Text Cviceni;
+	Cviceni.text("9.4");
+	//******************
+
+	/*BinarniStrom<int> BS;
+	BS.pridej(5);
+	BS.pridej(8);
+	BS.pridej(7);
+	BS.pridej(9);
+	BS.pridej(1);
+	BS.pridej(2);
+	BS.cti();*/
+
+	BinarniStrom<char> BS2;
+	BS2.pridej('A');
+	BS2.pridej('B');
+	BS2.pridej('C');
+	BS2.pridej('D');
+	BS2.pridej('E');
+	BS2.pridej('F');
+	BS2.pridej('G');
+	BS2.pridej('H');
+	BS2.pridej('I');
+	BS2.pridej('J');
+	BS2.pridej('K');
+	BS2.pridej('L');
+	BS2.cti();
+
+
+}
--- template.h
+++ template.h
@@ -1,28 +1,50 @@
 #pragma once
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
 #include <string>
 #include "vector"
 using namespace std;
+// ROZDÌLANÉ....ZATÍM VYHAZUJE CHYBY (pøeteèení)....
 
 template <class T>
-class GumovePole {
-	vector<T> hodnota;
+class BinarniStrom {
+	vector<T> v; // do gumového pole se ukládají hodnoty
+	T hodnota;/* = v[i];*/
+	T otec;/* = v[i / 2];*/
+	T Lsyn;/* = v[2 * i];*/
+	T Psyn;/* = v[2 * i + 1];*/
 public:
-	GumovePole() { hodnota; };
-	void pridej(T a) {
-		//cout << "Puvodni velikost gumoveho pole = " << hodnota.size() << endl;
-		hodnota.push_back(a);
-		cout << "Hodnota: " << a << " byla vlozena." << endl;
-		//cout << "Aktualni velikost gumoveho pole = " << hodnota.size() << endl;
+	BinarniStrom() { hodnota; otec; Lsyn; Psyn; };
+	void pridej(T x) {
+		hodnota = x;
+		v.push_back(hodnota);
 	};
-	void vypis(){
-		int j, i = 0;
-		for (j = 0; j < hodnota.size(); j++) {
-			i++;
-			cout << i << ". ulozena hodnota = " << hodnota[j] << endl;
+	void cti() {
+		int j = 0;
+		for (j; j < v.size(); j++) { // ještì poøešit pøeteèení pole
+			hodnota = v[j];
+			cout << "Index[" << j << "]\tHodnota[" << hodnota << "]";
+			if (j > 0) {
+				otec = v[j / 2];
+				if (v[(2 * j)+1]) {
+					Lsyn = v[(2 * j)+1];
+					if (v[(2 * j) + 2]) {
+						Psyn = v[(2 * j) + 2];
+					}
+					else Psyn = NULL;
+				}
+				else Lsyn = Psyn = NULL;
+				cout << "\tLevy syn: [" << (2 * j) + 1 << "] hodnota = " << v[(2 * j) + 1] << "\tPravy syn: [" << (2 * j) + 2 << "] hodnota = " << v[(2 * j) + 2] << "\tOtec: [" << ((j - 1) / 2) << "] hodnota = " << v[((j - 1) / 2)] << endl;
+			}
+			else {
+				Lsyn = v[1];
+				Psyn = v[2];
+				cout << "\tLevy syn: [" << 1 << "] hodnota = " << v[1] << "\tPravy syn: [" << 2 << "] hodnota = " << v[2] << endl;
+			}
 		}
-
+		if (j == v.size()) cout << "Vse..." << endl;
 	};
 };
+
